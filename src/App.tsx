3ed505289e433873fa272d5ac69c4a9993d6fdf0
kowasaur <kowasaur@gmail.com>
2021-07-10 17:52:41 +1000

    make better lol
    I edited this on my school laptop just a bit later after I made the previous commit but I only remembered to push it now

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,36 @@ import React, { useState, useRef } from "react";
 
 function App() {
   const [pi, setPi] = useState(1);
+  const [prePi, setPrePi] = useState(1);
   const [delay, setDelay] = useState(30);
   const [hasStarted, setHasStarted] = useState(false);
   const sliderRef = useRef<HTMLInputElement>(null);
 
   function calculate(denominator: number, doAdd: boolean) {
     // If I just used delay, it doesn't update
-    const lDelay = Number(sliderRef.current?.value) ** 2;
+    const lDelay = Number(sliderRef.current?.value ?? 30) ** 2;
 
     const newDen = denominator + 2;
 
     const change = doAdd ? 1 / newDen : -1 / newDen;
 
-    setPi(pi => pi + change);
+    setPi(pi => {
+      setPrePi(pi);
+      return pi + change;
+    });
 
     setTimeout(() => {
       calculate(newDen, !doAdd);
-      // It's NaN the first time
-    }, lDelay || 30 ** 2);
+    }, lDelay);
   }
 
   return (
     <>
-      <h1>{pi * 4}</h1>
+      <h1>{(pi + prePi) * 2}</h1>
       {hasStarted ? (
         <input
           type="range"
-          min="0.001"
+          min="0"
           max="50"
           onChange={d => setDelay(Number(d.target.value))}
           value={delay}
